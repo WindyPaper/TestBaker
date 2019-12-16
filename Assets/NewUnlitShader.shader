6955fae52e22f743c0645b5bf491ceb7057da274
Windy <windymaxs@gmail.com>
2019-12-17 00:11:11 +0800

    Init commit.

---
+++ Assets/NewUnlitShader.shader
@@ -0,0 +1,157 @@
+﻿Shader "Unlit/SH4"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_NormalTex("NormalTex", 2D) = "white" {}
+		_SH4CoffTexR("SH4CoffTexR", 2D) = "white" {}
+		_SH4CoffTexG("SH4CoffTexG", 2D) = "white" {}
+		_SH4CoffTexB("SH4CoffTexB", 2D) = "white" {}
+		_BakeDiffuse("Bake Diffuse", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+				float4 tangent : TANGENT;
+				float3 normal : NORMAL;
+				float4 lightmap_uv : TEXCOORD1;				
+				fixed4 color : COLOR;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+				//float3 tangent_ws : TEXCOORD2;
+				//float3 binormal_ws : TEXCOORD3;
+				float4 lightmap_uv : TEXCOORD4;
+            };
+
+			struct SH4Color
+			{
+				float4 c[3];
+			};
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+			sampler2D _NormalTex;
+			float4 _NormalTex_ST;
+			sampler2D _SH4CoffTexR;
+			float4 _SH4CoffTexR_ST;
+			sampler2D _SH4CoffTexG;
+			float4 _SH4CoffTexG_ST;
+			sampler2D _SH4CoffTexB;
+			float4 _SH4CoffTexB_ST;
+			sampler2D _BakeDiffuse;
+			float4 _BakeDiffuse_ST;
+
+			SH4Color ProjectOntoSH4Color(float3 dir, float3 color, float A0, float A1)
+			{
+				SH4Color sh;
+
+				float c0 = 0.282095f * A0;
+				float c1 = -0.488603f * dir.y * A1;
+				float c2 = 0.488603f * dir.z * A1;
+				float c3 = -0.488603f * dir.x * A1;
+
+				float4 sh_cof = float4(c0, c1, c2, c3);
+
+				sh.c[0] = color.r * sh_cof;
+				sh.c[1] = color.g * sh_cof;
+				sh.c[2] = color.b * sh_cof;
+
+				//// Band 0
+				//sh.c[0] = 0.282095f * A0 * color;
+
+				//// Band 1
+				//sh.c[1] = -0.488603f * dir.y * A1 * color;
+				//sh.c[2] = 0.488603f * dir.z * A1 * color;
+				//sh.c[3] = -0.488603f * dir.x * A1 * color;
+
+				return sh;
+			}
+
+			float3 SHDotProduct(in SH4Color a, in SH4Color b)
+			{
+				float3 result = 0.0f;
+
+				/*for (uint i = 0; i < 4; ++i)
+					result += a.c[i] * b.c[i];*/
+
+				for (uint i = 0; i < 3; ++i)
+				{
+					result += float3(a.c[0][i] * b.c[0][i], a.c[1][i] * b.c[1][i], a.c[2][i] * b.c[2][i]);
+				}
+
+				return result;
+			}
+
+			float3 EvalSH4Irradiance(float3 dir, SH4Color sh_color)
+			{
+				float Pi = 3.141592f;
+				float CosineA0 = Pi;
+				float CosineA1 = (2.0f * Pi) / 3.0f;
+
+				SH4Color dirSH = ProjectOntoSH4Color(dir, 1.0f, CosineA0, CosineA1);
+				return SHDotProduct(dirSH, sh_color);
+			}
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+
+				//float3 binormal = cross(normalize(v.normal), normalize(v.tangent.xyz)) * v.tangent.w;
+
+				o.lightmap_uv = v.lightmap_uv;
+
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv) / 3.14f;
+			
+				SH4Color sh_color;
+				sh_color.c[0] = tex2D(_SH4CoffTexR, i.lightmap_uv);
+				sh_color.c[1] = tex2D(_SH4CoffTexG, i.lightmap_uv);
+				sh_color.c[2] = tex2D(_SH4CoffTexB, i.lightmap_uv);
+
+				float3 normal_ts = normalize(tex2D(_NormalTex, float2(i.uv.x, 1 - i.uv.y)).xyz * 2 - 1);
+
+				float3 irradiance = EvalSH4Irradiance(normal_ts, sh_color);
+				col.rgb += irradiance;
+
+				float4 bake_diffuse = tex2D(_BakeDiffuse, i.lightmap_uv);
+				//float4 bake_diffuse = tex2D(_BakeDiffuse, float2(0.06f, 0.5f));
+
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+				//return float4(irradiance, 1.0);
+				//return bake_diffuse;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,284 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+using UnityEditor;
+using System.Reflection;
+using System.Threading;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    static IntPtr nativeLibraryPtr;
+
+    delegate int bake_scene(int number, int multiplyBy);
+    delegate bool init_cycles(int w, int h, [MarshalAs(UnmanagedType.LPStr)]string core_type);
+
+    delegate int unity_add_mesh(float[] vertex_array, float[] uvs_array, float[] lightmapuvs_array, float[] normal_array, int vertex_num,
+        int[] index_array, int[] mat_index, int triangle_num,
+        [MarshalAs(UnmanagedType.LPStr)]string[] mat_name, [MarshalAs(UnmanagedType.LPStr)]string[] diffuse_tex, int mat_num);
+
+    delegate int bake_lightmap();
+
+    void StartMission()
+    {
+        Debug.Log("Start...");
+
+        LoadDLL();
+
+        InitCycles();
+
+        SetAllMeshToCycles();
+
+        BakeLightMap();
+
+        Debug.Log("Finish...");
+    }
+
+    void Awake()
+    {
+        Debug.Log("Start...");
+
+        LoadDLL();
+
+        InitCycles();
+
+        SetAllMeshToCycles();
+
+        BakeLightMap();
+
+        Debug.Log("Finish...");
+    }
+
+    void LoadDLL()
+    {
+        if (nativeLibraryPtr != IntPtr.Zero) return;
+
+        foreach (System.Diagnostics.ProcessModule mod in System.Diagnostics.Process.GetCurrentProcess().Modules)
+        {
+            if (mod.FileName.Contains("Plugins")) //windows path
+            {
+                Debug.Log(Native.FreeLibrary(mod.BaseAddress));
+                Debug.Log(mod.FileName);
+            }
+        }
+
+        string dll_path = Application.dataPath + "/Plugins/";
+        string dll_file_name = "cycles.dll";
+        Native.LoadLibraryFlags flags = Native.LoadLibraryFlags.LOAD_LIBRARY_SEARCH_DEFAULT_DIRS;
+        string dll_full_name = dll_path + dll_file_name;
+        Native.AddDllDirectory(dll_path);
+        nativeLibraryPtr = Native.LoadLibraryEx(dll_full_name, IntPtr.Zero, flags);
+        
+        if (nativeLibraryPtr == IntPtr.Zero)
+        {
+            Debug.LogError("Failed to load native library. Path = " + dll_full_name + " Last Error Code = " + Marshal.GetLastWin32Error());
+
+            Debug.Log(Native.GetErrorMessage(Marshal.GetLastWin32Error()));
+        }
+    }
+
+    void InitCycles()
+    {
+
+        //try
+        //{
+        bool result = Native.Invoke<bool, init_cycles>(nativeLibraryPtr, Screen.width, Screen.height, "CPU"); // Should return the number 15.
+        Debug.Log(result);
+        //}
+        //catch (System.Exception e)
+        //{
+            //Debug.Log(e.Message);            
+        //}
+    }
+
+    private static List<MeshFilter> GetAllObjectsInScene()
+    {
+        List<MeshFilter> objectsInScene = new List<MeshFilter>();
+
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            MeshFilter mf = go.transform.GetComponent<MeshFilter>();
+            if (mf && go.active == true)
+            {
+                Debug.Log(go.name);
+                objectsInScene.Add(mf);
+            }
+        }
+        return objectsInScene;
+    }
+
+    void SetAllMeshToCycles()
+    {
+        List<MeshFilter> objs = GetAllObjectsInScene();
+
+        foreach(MeshFilter mf in objs)
+        {
+            Transform t = mf.transform;
+            Vector3 s = t.localScale;
+            Vector3 p = t.localPosition;
+            Quaternion r = t.localRotation;
+
+
+            int numVertices = 0;
+            Mesh m = mf.sharedMesh;
+            if (!m)
+            {
+                Debug.LogError("No mesh!");
+                continue;
+            }            
+
+            float[] vertex_array = new float[m.vertices.Length * 4];
+            foreach (Vector3 vv in m.vertices)
+            {
+                Vector3 v = t.TransformPoint(vv);
+                Debug.Log("pos = " + v.x + "  " + v.y + "  " + v.z);
+                //Vector3 v = (vv);
+                vertex_array[numVertices * 4] = -v.x;
+                vertex_array[numVertices * 4 + 1] = v.y;
+                vertex_array[numVertices * 4 + 2] = -v.z;
+                vertex_array[numVertices * 4 + 3] = 1.0f;
+
+                numVertices++;                
+            }
+            //sb.Append("\n");
+            int numNormal = 0;
+            float[] normal_array = new float[m.normals.Length * 4];
+            foreach (Vector3 nn in m.normals)
+            {
+                Vector3 v = r * nn;
+                //sb.Append(string.Format("vn {0} {1} {2}\n", -v.x, -v.y, v.z));
+                normal_array[numNormal * 4] = -v.x;
+                normal_array[numNormal * 4 + 1] = v.y;
+                normal_array[numNormal * 4 + 2] = -v.z;
+                normal_array[numNormal * 4 + 3] = 0.0f;
+
+                numNormal++;
+            }
+            //sb.Append("\n");
+            int numUVs = 0;
+            float[] uvs_array = new float[m.uv.Length * 2];
+            foreach (Vector3 v in m.uv)
+            {
+                //sb.Append(string.Format("vt {0} {1}\n", v.x, v.y));
+                uvs_array[numUVs * 2] = v.x;
+                uvs_array[numUVs * 2 + 1] = v.y;
+
+                numUVs++;
+            }
+
+            float[] lightmapuv_array = new float[m.uv.Length * 2]; //hack 防止没有lightmap崩溃
+            int numLightmapuv = 0;
+            if (m.uv2.Length > 0)
+            {                       
+                foreach (Vector3 v in m.uv2)
+                {
+                    //sb.Append(string.Format("vt {0} {1}\n", v.x, v.y));
+                    if(v.x > 1.0f || v.x < -0.0001f)
+                    {
+                        Debug.LogError("Uv error!");
+                    }
+
+                    lightmapuv_array[numLightmapuv * 2] = v.x;
+                    lightmapuv_array[numLightmapuv * 2 + 1] = v.y;
+
+                    numLightmapuv++;
+                }
+            }
+            //Debug.Log("Lightmap uv num = " + numLightmapuv);
+            Debug.Log("vertice num = " + numVertices);
+            if (numLightmapuv != m.uv.Length)
+            {
+                Debug.LogError("numLightmapuv != m.uv.Length");
+            }
+
+            Material[] mats = mf.GetComponent<Renderer>().sharedMaterials;
+            int mat_num = mats.Length;
+            string[] mat_name = new string[mat_num];
+            string[] diffuse_tex_name = new string[mat_num];
+            for(int i = 0; i < mat_num; ++i)
+            {
+                mat_name[i] = mats[i].name;
+                diffuse_tex_name[i] = Application.dataPath + "/../" + AssetDatabase.GetAssetPath(mats[i].mainTexture);
+                Debug.Log("texture full path = " + Application.dataPath + "/../" + AssetDatabase.GetAssetPath(mats[i].mainTexture));
+            }
+
+            //int mat_num = m.subMeshCount;
+            int triangle_num = 0;            
+            for (int material = 0; material < m.subMeshCount; material++)
+            {
+                int[] triangles = m.GetTriangles(material);
+                triangle_num += triangles.Length;
+            }
+
+            int[] index_array = new int[triangle_num * 3];
+            int[] index_mat_array = new int[triangle_num];
+            Debug.Log("triangle_num num = " + triangle_num);
+            int index_i = 0;            
+            for (int material = 0; material < m.subMeshCount; material++)
+            {
+                int[] triangles = m.GetTriangles(material);
+                
+                for (int i = 0; i < triangles.Length; i += 3)
+                {
+                    //revert wind
+                    index_array[index_i * 3] = triangles[i+1];
+                    index_array[index_i * 3 + 1] = triangles[i+2];
+                    index_array[index_i * 3 + 2] = triangles[i+0];
+                    index_mat_array[index_i] = material;
+
+                    ++index_i;
+                }
+            }
+
+            Native.Invoke<int, unity_add_mesh>(nativeLibraryPtr,
+                vertex_array, uvs_array, lightmapuv_array, normal_array, numVertices,
+                index_array, index_mat_array, triangle_num, mat_name, diffuse_tex_name, mat_num);
+        }
+    }
+
+    void BakeLightMap()
+    {
+        Native.Invoke<int, bake_lightmap>(nativeLibraryPtr);
+    }
+    
+    void Update()
+    {
+       
+    }
+
+    public static void UnloadDLLAndExitThread()
+    {
+        Debug.Log("FreeLibraryAndExitThread");
+        Native.FreeLibraryAndExitThread(nativeLibraryPtr, 0);        
+    }
+
+    void OnApplicationQuit()
+    {
+        //if (nativeLibraryPtr == IntPtr.Zero) return;
+        Debug.Log("OnApplicationQuit");
+
+        //foreach (System.Diagnostics.ProcessModule mod in System.Diagnostics.Process.GetCurrentProcess().Modules)
+        //{
+        //    if(mod.FileName.Contains("Plugins")) //windows path
+        //    {
+        //        Debug.Log(Native.FreeLibrary(mod.BaseAddress));
+        //        Debug.Log(mod.FileName);
+        //    }                        
+        //}
+
+        //Make sure DLL is unload!
+        //while (Native.FreeLibrary(nativeLibraryPtr))
+        //{
+        //    Debug.Log("Free DLL!");
+        //}
+        Thread t = new Thread(UnloadDLLAndExitThread);
+        t.Start();
+        //UnloadDLLAndExitThread();
+
+        //if(Native.FreeLibrary(nativeLibraryPtr) == false)
+        //{
+        //    Debug.Log("Make sure DLL is unload!");
+        //}
+    }
+}
